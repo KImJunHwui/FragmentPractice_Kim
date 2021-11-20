54cc2e020cc813b9f72515508df6991bfe42e3af
KJH <daeding7@naver.com>
2021-11-20 23:27:41 +0900

    프래그먼트 kt <-> xml 파일 결합 작업

---
+++ app/src/main/java/com/kim/fragmentpractice_kim/fragments/FirstFragment.kt
@@ -0,0 +1,19 @@
+package com.kim.fragmentpractice_kim.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kim.fragmentpractice_kim.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        inflater.inflate(R.layout.fragment_first, container, false)
+    }
+}
